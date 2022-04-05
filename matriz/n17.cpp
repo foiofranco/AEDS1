0715de565ba0dfb16e3506607530630805ff917e
João Franco <88912564+foiofranco@users.noreply.github.com>
2022-04-04 23:02:19 -0300

    Rename n17.cpp to matriz/n17.cpp

--- matriz/n17.cpp
+++ matriz/n17.cpp
@@ -1,5 +1,5 @@
-// AEDS I - Arrays | Exercício nº 17
-// Nome: João Francisco da Silva Franco | Matrícula: 2021008720 | Data: 08/09/2021;
+// AEDS I - Arrays | ExercÃ­cio nÂº 17
+// Nome: JoÃ£o Francisco da Silva Franco | MatrÃ­cula: 2021008720 | Data: 08/09/2021;
 
 // Bibliotecas:
 #include <iostream>
@@ -8,24 +8,24 @@
 using namespace std;
 
 void determinante(){
-	// Variáveis:
+	// VariÃ¡veis:
 	float matriz[3][3], resultante = 0;
-	// Execuções:
+	// ExecuÃ§Ãµes:
 	for (int linha = 0; linha < 3; linha++){
 		for (int coluna = 0; coluna < 3; coluna ++){
-			cout << "- Insira o valor da " << linha+1 << "ª linha da " << coluna+1 << "ª coluna: ";
+			cout << "- Insira o valor da " << linha+1 << "Âª linha da " << coluna+1 << "Âª coluna: ";
 			cin >> matriz[linha][coluna];
 		}
 	}
 	resultante = (matriz[0][0] * matriz[1][1] * matriz[2][2]) + (matriz[0][1] * matriz[1][2] * matriz[2][0]) + (matriz[0][2] * matriz[1][0] * matriz[2][1]) - (matriz[0][2] * matriz[1][1] * matriz[2][0]) - (matriz[0][0] * matriz[1][2] * matriz[2][1]) - (matriz[0][1] * matriz[1][0] * matriz[2][2]);
-	cout << endl << "O resultante da matriz em questão corresponde a: " << resultante;
+	cout << endl << "O resultante da matriz em questÃ£o corresponde a: " << resultante;
 }
 
 int main(){
-	// Caractéres especiais:
+	// CaractÃ©res especiais:
 	setlocale (LC_ALL, "Portuguese");
-	// Execuções:
-	cout << "O programa em questão calcula o determinante de uma matriz 3x3." << endl << endl;
+	// ExecuÃ§Ãµes:
+	cout << "O programa em questÃ£o calcula o determinante de uma matriz 3x3." << endl << endl;
 	determinante();
 	return 0;
 }
